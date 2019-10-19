61bef6cfb8538c6684034be352f114bace57ac64
rachit2501 <rac.sri25@gmail.com>
2019-10-20 02:03:18 +0530

    breaking of template done

--- clientRachit/src/App.js
+++ clientRachit/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Footer from './components/footer';
 import Header from './components/header';
 import Home from './components/home';
+
 function App() {
   return (
     <React.Fragment>
---
+++ clientRachit/src/components/features.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export class breadcrumb extends Component {
+    render() {
+        return (
+            <section className="breadcrumb breadcrumb_bg">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="breadcrumb_iner text-center">
+                <div className="breadcrumb_iner_item">
+                  <h2>featured</h2>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+        )
+    }
+}
+
+export default breadcrumb;
